d20bc28a6b5ae45b0b5bbd7a9267266102ff9c65
Null3x5 <izuniga485@gmail.com>
2016-10-30 18:46:01 -0700

    error issue

--- DataCollector.js
+++ DataCollector.js
@@ -13,8 +13,15 @@ DataCollector.CollectData = function(callback,url){
 
     res.on('end', function(){
         var matchData = JSON.parse(body);
-
-        callback(matchData.matches);
+        if(matchData.matches != undefined){
+            var matchData = JSON.parse(body);
+            callback(matchData.matches);
+        }else{
+            var err = JSON.parse(body);
+            console.log('error code in response body :' + body);
+            console.log('timeout for 10000');
+            setTimeout(DataCollector.CollectData(callback,url),1000);
+        }      
     });
 }).on('error', function(e){
       console.log("Got an error: ", e);
